a1e7eec13e200ac9049378b4525be15487e2ac52
Siddana gowda <126750766+Siddanagowda@users.noreply.github.com>
2024-11-17 10:08:12 +0530

    solution

---
+++ ShortestSubarraywithSumatLeastK.java
@@ -0,0 +1,22 @@
+public class Solution {
+    public int shortestSubarray(int[] nums, int k) {
+        int n = nums.length;
+        long[] prefixSum = new long[n + 1];
+        
+        for (int i = 0; i < n; i++) {
+            prefixSum[i + 1] = prefixSum[i] + nums[i];
+        }
+        Deque<Integer> deque = new LinkedList<>();
+        int result = Integer.MAX_VALUE;
+        for (int i = 0; i <= n; i++) {
+            while (!deque.isEmpty() && prefixSum[i] - prefixSum[deque.peekFirst()] >= k) {
+                result = Math.min(result, i - deque.pollFirst());
+            }
+            while (!deque.isEmpty() && prefixSum[i] <= prefixSum[deque.peekLast()]) {
+                deque.pollLast();
+            }
+            deque.offerLast(i);
+        }
+        return result == Integer.MAX_VALUE ? -1 : result;
+    }
+}
